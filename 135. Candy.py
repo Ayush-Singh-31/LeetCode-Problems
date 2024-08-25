b114d2c386f0cbe9e60553d13bfc521ee4af61c8
Ayush <sayush5191@gmail.com>
2024-08-25 10:55:57 +0530

    Update

--- 135. Candy.py
+++ 135. Candy.py
@@ -1,5 +1,5 @@
 """
-here are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
 
 You are giving candies to these children subjected to the following requirements:
 
